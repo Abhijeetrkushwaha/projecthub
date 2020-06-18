3c7f7d04fa8901a51e9b2060e781fb0f84c10bd0
Abhijeet Kushwaha <abhijeetrkushwaha28@gmail.com>
2020-06-18 08:54:24 +0530

    Layout done

---
+++ src/components/project/ProjectDetails.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function ProjectDetails() {
+    return (
+        <div className="container section project-details">
+            <div className="card z-depth-2">
+                <div className="card-content">
+                    <span className="card-title"></span>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Commodi totam voluptate animi optio ut sit consectetur maiores est qui quidem recusandae iusto dolores iste eligendi deleniti nesciunt nisi, tempore fuga!</p>
+                </div>
+                <div className="card-action grey lighten-4 grey-text">
+                    <div className="">Posted By Abhijeet</div>
+                    <div>18 of June</div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ProjectDetails
---
+++ src/components/project/ProjectSummary.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const ProjectSummary = () => {
+    return (
+      <div className="card z-depth-2 project-summary center">
+        <div className="card-content grey-text text-darken-3">
+          <span className="card-title">Project Title</span>
+          <p>Posted by Abhijeet</p>
+          <p className="grey-text">18th June, 8am</p>
+        </div>
+      </div>
+    );
+}
+
+export default ProjectSummary;
