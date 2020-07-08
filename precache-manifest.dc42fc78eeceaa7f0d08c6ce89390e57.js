cc73461fedab240f706f6f7a76fbc444cdc87487
Abhijeet Kushwaha <abhijeetrkushwaha28@gmail.com>
2020-07-08 15:36:36 +0530

    Updates

---
+++ precache-manifest.dc42fc78eeceaa7f0d08c6ce89390e57.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6292193647852b6245ca6efee8530fc8",
+    "url": "/projecthub/index.html"
+  },
+  {
+    "revision": "73e82a6e93abdd8405a1",
+    "url": "/projecthub/static/css/main.cf3c3f7b.chunk.css"
+  },
+  {
+    "revision": "511200a533bcac9cd0fb",
+    "url": "/projecthub/static/js/2.97c4bd5d.chunk.js"
+  },
+  {
+    "revision": "0135287500bd9d64203d07b4f33f971f",
+    "url": "/projecthub/static/js/2.97c4bd5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73e82a6e93abdd8405a1",
+    "url": "/projecthub/static/js/main.f6477ec8.chunk.js"
+  },
+  {
+    "revision": "b22908e909c842b10c81",
+    "url": "/projecthub/static/js/runtime-main.411ac0a6.js"
+  }
+]);
